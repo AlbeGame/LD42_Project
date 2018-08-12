8532f90c10df56523f1df968711252839ac91514
AlbertoDesktop <alberto.paparella.88@gmail.com>
2018-08-12 18:27:10 +0200

    Audio clips

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AudioController : MonoBehaviour {
+
+    AudioSource sorce;
+    public List<AudioClip> Clips = new List<AudioClip>();
+
+	// Use this for initialization
+	void Start () {
+        sorce = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
